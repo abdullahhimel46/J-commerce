14db56cc20f2c4550e5a41254729f95fbeeb8fed
Rezaur Rahman <rezaur.official@gmail.com>
2026-02-14 21:15:51 +0600

    Rezaur: Initialized fresh project

---
+++ src/main/java/com/rezaur/main/JCommerceApplication.java
@@ -0,0 +1,13 @@
+package com.rezaur.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JCommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JCommerceApplication.class, args);
+    }
+
+}
